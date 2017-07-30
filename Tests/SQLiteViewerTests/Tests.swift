fe91485b25e18ecf9bbb77a2174ec183979dab4d
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2017-07-30 20:11:26 +0400

    Added test target

---
+++ Tests/SQLiteViewerTests/Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteViewerTests.swift
+//  SQLiteViewerTests
+//
+//  Created by Orkhan Alikhanov on 7/30/17.
+//  Copyright © 2017 BiAtoms. All rights reserved.
+//
+
+import XCTest
+@testable import SQLiteViewer
+
+class SQLiteViewerTests: XCTestCase {
+    
+    func testExample() {
+        
+    }
+}
