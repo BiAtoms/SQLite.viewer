4d4fb16f14843fa534271de0abcadb0343a4ed22
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2017-07-30 19:17:31 +0400

    Fixed project errors

---
+++ Sources/SQLiteViewer.h
@@ -0,0 +1,19 @@
+//
+//  SQLiteViewer.h
+//  SQLiteViewer
+//
+//  Created by Orkhan Alikhanov on 7/30/17.
+//  Copyright © 2017 BiAtoms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLiteViewer.
+FOUNDATION_EXPORT double SQLiteViewerVersionNumber;
+
+//! Project version string for SQLiteViewer.
+FOUNDATION_EXPORT const unsigned char SQLiteViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteViewer/PublicHeader.h>
+
+
